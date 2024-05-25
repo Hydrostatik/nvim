9732adfa71efb84bdf5925aa8f3226004f04a5a6
Hydrostatik <40757442+Hydrostatik@users.noreply.github.com>
2024-05-25 00:23:39 -0400

    add some initial configurations

---
+++ lua/options.lua
@@ -0,0 +1,18 @@
+-- assign leader key to space
+-- you can think of <leader> in keybindings as space
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.clipboard = 'unnamedplus'
+
+vim.o.number = true
+
+vim.o.signcolumn = 'yes'
+
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
+vim.o.updatetime = 300
+
+vim.o.termguicolors = true
+vim.o.mouse = 'a'
