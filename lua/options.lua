28a60ef0571d0b7e379daf13348ef014659ee2d8
Hydrostatik <40757442+Hydrostatik@users.noreply.github.com>
2024-05-26 16:58:15 -0400

    Add autopairs and format on save

--- lua/options.lua
+++ lua/options.lua
@@ -1,13 +1,13 @@
 -- assign leader key to space
 -- you can think of <leader> in keybindings as space
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 vim.o.number = true
 
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 
 vim.o.expandtab = true
 vim.o.tabstop = 2
@@ -16,5 +16,17 @@ vim.o.shiftwidth = 2
 vim.o.updatetime = 300
 
 vim.o.termguicolors = true
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
+-- Format on save
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        vim.lsp.buf.format({ async = false, id = args.data.client_id })
+      end,
+    })
+  end,
+})
