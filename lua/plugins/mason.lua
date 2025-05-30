99ba238983289876b58e548165ec4fd62b9d7f6e
Hydrostatik <40757442+Hydrostatik@users.noreply.github.com>
2025-05-30 02:25:23 -0400

    Fix rust completions

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,62 +1,51 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "hls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "hls" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.rust_analyzer.setup({
-				capabilities = capabilities,
-				settings = {
-					["rust-analyzer"] = {
-						checkOnSave = {
-							command = "clippy",
-						},
-					},
-				},
-			})
+      lspconfig.hls.setup({
+        capabilities = capabilities,
+        filetypes = { "haskell", "lhaskell", "cabal" },
+      })
 
-			lspconfig.hls.setup({
-				capabilities = capabilities,
-				filetypes = { "haskell", "lhaskell", "cabal" },
-			})
+      lspconfig.emmet_language_server.setup({})
 
-			lspconfig.emmet_language_server.setup({})
+      vim.diagnostic.config({
+        underline = true,
+        signs = true,
+        float = {
+          show_header = true,
+          source = "if_many",
+          border = "rounded",
+          focusable = false,
+        },
+      })
 
-			vim.diagnostic.config({
-				underline = true,
-				signs = true,
-				float = {
-					show_header = true,
-					source = "if_many",
-					border = "rounded",
-					focusable = false,
-				},
-			})
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
-		end,
-	},
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
+    end,
+  },
 }
