a4bff5a21afe66117271dc6480d26661ccc3db66
Hydrostatik <40757442+Hydrostatik@users.noreply.github.com>
2025-05-30 01:44:51 -0400

    fix spaces config

--- lua/options.lua
+++ lua/options.lua
@@ -12,6 +12,8 @@ vim.o.signcolumn = "yes"
 vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+vim.o.smartindent = true
+vim.o.softtabstop = 2
 
 vim.o.updatetime = 300
 
@@ -20,13 +22,13 @@ vim.o.mouse = "a"
 
 -- Format on save
 vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
-  callback = function(args)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = args.buf,
-      callback = function()
-        vim.lsp.buf.format({ async = false, id = args.data.client_id })
-      end,
-    })
-  end,
+	group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+	callback = function(args)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = args.buf,
+			callback = function()
+				vim.lsp.buf.format({ async = false, id = args.data.client_id })
+			end,
+		})
+	end,
 })
